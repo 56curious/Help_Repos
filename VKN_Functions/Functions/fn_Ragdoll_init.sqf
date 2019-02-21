e04288e44c6e7094767333e35180a4127dd2b719
56curious <41159450+56curious@users.noreply.github.com>
2019-02-21 15:58:38 +0000

    VKN_Functions

---
+++ VKN_Functions/Functions/fn_Ragdoll_init.sqf
@@ -0,0 +1,40 @@
+/*
+  Author: Diwako - Heavily Modifed by Curious
+
+  Description:
+  Adds ragdolling for players upon going unconscious
+
+  Parameter(s):
+  none
+
+  Examples:
+  (begin)
+    [] call VKN_fnc_Ragdoll_init;
+  (end)
+
+  Returns:
+    none
+*/
+
+// no need to initialise ragdolling twice!
+
+if (!isNil 'VKN_ragdoll_ragdollRunning') exitWith {};
+VKN_ragdoll_ragdollRunning = true;
+
+["ace_unconscious", {
+  params [["_unit", objNull],["_state", false]];
+  if (!VKN_ragdoll_ragdolling) exitWith {}; // ragdolling if it's active
+  if ((!(isPlayer _unit) && {!VKN_ragdoll_ai})) exitWith {}; // only ragdoll players and only ragdolling AI if that option is set active
+  if (_state && {(isNull objectParent _unit) && {!([_unit] call ace_medical_fnc_isBeingCarried) && {!([_unit] call ace_medical_fnc_isBeingDragged)}}}) then {
+    // ragdoll unit
+    _unit setUnconscious true;
+  };
+  if (!_state) then {
+    // unit woke up before ragdolling was finished
+    _unit setUnconscious false;
+  };
+}] call CBA_fnc_addEventHandler;
+
+["CAManBase", "AnimChanged", {
+  _this call VKN_fnc_Ragdoll_animChangedEH;
+}] call CBA_fnc_addClassEventHandler;
---
+++ VKN_Functions/config.cpp
@@ -0,0 +1,88 @@
+class CfgPatches {
+    class VKN_functions {
+        name = VKN_Functions;
+        author = "Curious";
+		version = 1.0.0;
+		versionStr = "1.0.0";
+		versionAr[] = {0,1,0};
+
+		units[] = {};
+
+		requiredAddons[] =
+		{
+			"A3_UI_F",
+			"A3_UI_F_Curator",
+			"A3_Functions_F",
+			"A3_Functions_F_Curator",
+			"A3_Functions_F_Mark"
+		};
+	};
+};
+
+class CfgFunctions {
+	class full_mission_load {
+		class functions {
+			file = "\VKN_Functions\Functions";
+			class load{};
+		};
+	};
+
+
+	class VKN {
+		class functions {
+			file = "\VKN_functions\functions";
+			class icons{postInit = 1;};
+
+			class strategicMapInit{};
+			class strategicMapPreInit{postInit = 1;};
+			class strategicMapOpen{};
+			class strategicMapRegisterMission{};
+			class sunEval{};
+
+			class emp_tgt_sfx{};
+			class emp_viz_eff_emp{};
+			class empConfigObj{};
+			class empEffect{};
+			class empSparkEffect{};
+			class empSparky{};
+			class empStarter{};
+			class fireEMP{};
+			class fireEMPWithTFRJam{};
+
+			class monsoon_alias_hunt{};
+			class monsoon_camera_shake{};
+			class monsoon_effect{};
+			class monsoon_rain_drop{};
+			class monsoon_start{};
+			class monsoon_thunder_storm{};
+
+			class Ragdoll_init{postInit = 1;};
+			class Ragdoll_animChangedEH{};
+		};
+	};
+};
+
+class Extended_PreInit_EventHandlers {
+	class VKN_Settings_preInit {
+		CUR_missionStart = call compile preprocessfilelinenumbers "\VKN_Functions\XEH_preInit.sqf";
+	};
+};
+/*
+class ctrlMenuStrip;
+class display3DEN {
+    class Controls {
+        class MenuStrip: ctrlMenuStrip {
+            class Items {
+                class Tools {
+                    items[] += {"Separator", "Mission_Template_Tool"};
+                };
+                class Mission_Template_Tool {
+                    text = "Mission Template Tool";
+                    Picture = "\VKN_Misc\VikingLogo128.paa";
+                    action = "[] call VKN_fnc_missionTemplateTool;";
+                };
+            };
+        };
+    };
+};
+*/
